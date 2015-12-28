7ac546f7db89b3dcfb17b1fca0c789befdebb382
campestrini <eduardocampestrini@yahoo.com.br>
2015-12-28 08:18:54 -0200

    Committing classes for previous project.

---
+++ App/App/Rule.cs
@@ -0,0 +1,46 @@
+﻿using System;
+
+namespace App
+{
+    public class Rule
+    {
+        public const string INVALID_STARTING_DIGIT = "Starting point must have a value.";
+        public const string INVALID_LENGTH = "Legth must have a value greater than zero.";
+
+        public int[] startingDigit { get; }
+        public int[] length { get; }
+
+        public Rule(int[] startingDigit, int[] length)
+        {
+            this.validateStartingDigit(startingDigit);
+            this.validateLength(length);
+
+            this.startingDigit = startingDigit;
+            this.length = length;
+        }
+
+        private void validateStartingDigit(int[] startingPoint)
+        {
+            if (startingPoint == null || startingPoint.Length == 0)
+            {
+                throw new ArgumentException(INVALID_STARTING_DIGIT);
+            }
+        }
+
+        private void validateLength(int[] length)
+        {
+            if (length == null || length.Length == 0)
+            {
+                throw new ArgumentException(INVALID_LENGTH);
+            }
+
+            foreach (int element in length)
+            {
+                if (element <= 0)
+                {
+                    throw new ArgumentException(INVALID_LENGTH);
+                }
+            }
+        }
+    }
+}
---
+++ App/App/Validator.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace App
+{
+    public class Validator
+    {
+        public const string INVALID_FLAG = "Credit card flag must have a value.";
+        public const string INVALID_RULE = "Rule can't be null.";
+        public const string FLAG_NOT_FOUND = "There is no rule for this flag.";
+
+        public Dictionary<string, Rule> rules;
+
+        public Validator()
+        {
+            this.rules = new Dictionary<string, Rule>();
+        }
+
+        public bool hasRules()
+        {
+            return Convert.ToBoolean(rules.Count);
+        }
+
+        public void addRule(string flag, Rule rule)
+        {
+            if (String.IsNullOrEmpty(flag))
+            {
+                throw new ArgumentException(INVALID_FLAG);
+            }
+            else if (rule == null)
+            {
+                throw new ArgumentException(INVALID_RULE);
+            }
+
+            rules.Add(flag, rule);
+        }
+
+        public Rule getRule(string flag)
+        {
+            if (!rules.ContainsKey(flag))
+            {
+                throw new KeyNotFoundException(FLAG_NOT_FOUND);
+            }
+
+            return rules[flag];
+        }
+
+    }
+}
