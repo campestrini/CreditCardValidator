0b90691e3689c8dad32f022292f45176abf02406
campestrini <eduardocampestrini@yahoo.com.br>
2015-12-28 08:22:02 -0200

    Commiting testing files from previous project.

---
+++ App/Test/RuleTest.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using App;
+
+namespace Test
+{
+    [TestClass]
+    public class RuleTest
+    {
+        private static int[] _validStartingDigit = { 6011 };
+        private static int[] _validLenght = { 13, 16 };
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), Rule.INVALID_STARTING_DIGIT)]
+        public void RuleConstructorWithInvalidStartingPointShouldThrowAnException()
+        {
+            int[] startingDigit = null;
+            Rule rule = new Rule(startingDigit, _validLenght);
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), Rule.INVALID_LENGTH)]
+        public void RuleConstructorWithInvalidLengthShouldThrowAnException()
+        {
+            int[] length = null;
+            Rule rule = new Rule(_validStartingDigit, length);
+        }
+        [TestMethod]
+        public void RuleShouldBeConstructedProperly()
+        {
+            Rule rule = new Rule(_validStartingDigit, _validLenght);
+            Equals(_validStartingDigit, rule.startingDigit);
+            Equals(_validLenght, rule.length);
+        }
+    }
+}
---
+++ App/Test/ValidatorTest.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Collections.Generic;
+using App;
+
+namespace Test
+{
+    [TestClass]
+    public class ValidatorTest
+    {
+        private static Validator _validator = new Validator();
+        private static int[] _startingDigit = { 23 };
+        private static int[] _length = { 16 };
+        private static string _flag = "Visa";
+
+        private static Rule _rule = new Rule(_startingDigit, _length);
+
+        [TestMethod]
+        public void ValidatorShouldBeInitializedWithNoRules()
+        {
+            Assert.IsFalse(_validator.hasRules());
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), Validator.INVALID_FLAG)]
+        public void AddRuleWithInvalidFlagShouldThrowException()
+        {
+            _validator.addRule(null, _rule);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(ArgumentException), Validator.INVALID_FLAG)]
+        public void AddInvalidRuleShouldThrowException()
+        {
+            _validator.addRule(_flag, null);
+
+        }
+
+        [TestMethod]
+        [ExpectedException(typeof(KeyNotFoundException), Validator.FLAG_NOT_FOUND)]
+        public void ValidatorShouldThrowExceptionWhenRuleNotExist()
+        {
+            _validator.getRule("ABC");
+        }
+
+        [TestMethod]
+        public void RuleShouldBeStoredProperly()
+        {
+            _validator.addRule(_flag, _rule);
+            Rule rule = _validator.getRule(_flag);
+            Equals(_rule.startingDigit, rule.startingDigit);
+            Equals(_rule.length, rule.length);
+        }
+        [TestMethod]
+        public void ValidatorShouldThrowExeceptionWithInvalidCcNumber()
+        {
+            
+        }
+    }
+}
